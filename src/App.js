323fe871a59970ac838e6d2cd1352c0b47d1c889
Kastureema <kastureemasrm@gmail.com>
2024-01-30 08:16:12 -0800

    added text to show move # and allow sorting the moves

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function Board({xIsNext, squares, onPlay}) {
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      </div> 
     </>
   );
 }
@@ -56,6 +56,7 @@ function Board({xIsNext, squares, onPlay}) {
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [ascending, setAscending] = useState(0);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
@@ -87,9 +88,11 @@ export default function Game() {
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+        <span className="info-span">You are at move #{currentMove}</span>
       </div>
       <div className="game-info">
-        <ol>{moves}</ol>
+        <button className="toggle-btn" onClick={() => setAscending(!ascending)}>Reverse</button>
+        <ol>{!ascending ? moves : moves.reverse()}</ol>
       </div>
     </div>
   )
