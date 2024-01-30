fd696e72f4c4fc9f951c26c4ff58f409ea80c6ad
Kastureema <kastureemasrm@gmail.com>
2024-01-30 06:44:37 -0800

    Updates

---
+++ precache-manifest.3972371871a00a50543020b2d92acd76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d12ef670efc9700f14f5fe37550de61",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "800be8f75d2e7ab59e51",
+    "url": "/tic-tac-toe/static/css/main.8a63c9a3.chunk.css"
+  },
+  {
+    "revision": "938cdb0f8affad69108e",
+    "url": "/tic-tac-toe/static/js/2.74ddf166.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/tic-tac-toe/static/js/2.74ddf166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800be8f75d2e7ab59e51",
+    "url": "/tic-tac-toe/static/js/main.8dd49318.chunk.js"
+  },
+  {
+    "revision": "c42334c3623e1c9838af",
+    "url": "/tic-tac-toe/static/js/runtime-main.09355417.js"
+  }
+]);
